fa565adad86b2fd4a12291ece93a3c5000bfe7c9
Christian Pontesegger <christian.pontesegger@web.de>
2021-04-15 12:16:12 +0200

    remove target binaries

--- libraries/org.jruby/target/classes/jruby/java/java_ext.rb
+++
@@ -1,8 +0,0 @@
-# Extensions to Java classes
-
-# These are loads so they don't pollute LOADED_FEATURES
-load 'jruby/java/java_ext/java.lang.rb'
-load 'jruby/java/java_ext/java.util.rb'
-load 'jruby/java/java_ext/java.util.regex.rb'
-load 'jruby/java/java_ext/java.io.rb'
-load 'jruby/java/java_ext/java.net.rb'
